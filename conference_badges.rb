41d5d2e8ec2a1faf0c5e33b0295322c31f3b0515
Thompson Plyler <nealok.g@gmail.com>
2018-11-03 03:34:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -30,9 +30,9 @@ end
 def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
     puts badge
+  end
   assign_rooms(attendees).each do |assignment|
     puts assignment
-  end
   end
 end
   
